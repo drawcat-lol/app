4634e69ac18c3e4d0071fbdc6de4d6f469883ca2
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-07-15 23:57:16 +0530

    add pagination component

--- src/components/explore.tsx
+++ src/components/explore.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Heart, Share2 } from "lucide-react";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function Explore() {
     return (
@@ -16,42 +25,70 @@ export default function Explore() {
                 </span>
             </div>
 
-            <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 overflow-hidden rounded-2xl gap-[1px] border bg-border shadow-2xl">
-                {[...Array(8)].map((item, index) => (
-                    <div
-                        className="w-full flex flex-col text-start overflow-hidden group bg-white relative"
-                        key={index}
-                    >
-                        <div className="overflow-hidden">
-                            <Image
-                                className="w-full aspect-square group-hover:scale-105 duration-175"
-                                src={"/cat_drawing.png"}
-                                width={256}
-                                height={256}
-                                alt="cat drawing"
-                            />
-                        </div>
-                        <div className="p-4 flex flex-col gap-2 absolute bg-white bottom-0 translate-y-full group-hover:translate-y-0 duration-175 w-full border-t">
-                            <span className="font-bold font-display text-2xl">
-                                some cat
-                            </span>
-                            <div className="flex justify-between items-end">
-                                <div className="flex gap-2">
-                                    <Button variant={"outline"} size={"icon"}>
-                                        <Heart />
-                                    </Button>
-                                    <Button variant={"outline"} size={"icon"}>
-                                        <Share2 />
-                                    </Button>
-                                </div>
-
-                                <span className="text-xs font-semibold opacity-50">
-                                    2 days ago
+            <div className="flex flex-col mt-8 shadow-2xl border overflow-hidden rounded-2xl w-full">
+                <div className="p-2 border-b flex justify-start w-full">
+                    <div className="w-fit">
+                        <Pagination>
+                            <PaginationContent>
+                                <PaginationItem>
+                                    <PaginationPrevious href="#" />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationLink href="#">1</PaginationLink>
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationEllipsis />
+                                </PaginationItem>
+                                <PaginationItem>
+                                    <PaginationNext href="#" />
+                                </PaginationItem>
+                            </PaginationContent>
+                        </Pagination>
+                    </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-[1px] bg-border">
+                    {[...Array(8)].map((item, index) => (
+                        <div
+                            className="w-full flex flex-col text-start overflow-hidden group bg-white relative"
+                            key={index}
+                        >
+                            <div className="overflow-hidden">
+                                <Image
+                                    className="w-full aspect-square group-hover:scale-105 duration-175"
+                                    src={"/cat_drawing.png"}
+                                    width={256}
+                                    height={256}
+                                    alt="cat drawing"
+                                />
+                            </div>
+                            <div className="p-4 flex flex-col gap-2 absolute bg-white bottom-0 translate-y-full group-hover:translate-y-0 duration-175 w-full border-t">
+                                <span className="font-bold font-display text-2xl">
+                                    some cat
                                 </span>
+                                <div className="flex justify-between items-end">
+                                    <div className="flex gap-2">
+                                        <Button
+                                            variant={"outline"}
+                                            size={"icon"}
+                                        >
+                                            <Heart />
+                                        </Button>
+                                        <Button
+                                            variant={"outline"}
+                                            size={"icon"}
+                                        >
+                                            <Share2 />
+                                        </Button>
+                                    </div>
+
+                                    <span className="text-xs font-semibold opacity-50">
+                                        2 days ago
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
