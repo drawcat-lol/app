044dfed8c601e7d8c8e08e031c52102a96976dd5
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-07-17 01:32:08 +0530

    feat: add slack oauth - use dynamic path for oauth providers

---
+++ src/app/auth/login/[provider]/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import suapbase from "@/utils/supabase";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import type { Provider } from "@supabase/supabase-js";
+
+export default function Page() {
+    const params = useParams();
+    const provider = params.provider as Provider;
+
+    useEffect(() => {
+        const valid: Provider[] = ["discord", "slack_oidc"];
+        if (!valid.includes(provider)) return;
+
+        suapbase.auth.signInWithOAuth({
+            provider: provider,
+            options: {
+                redirectTo: `${window.location.origin}/auth/callback`,
+            },
+        });
+    }, [provider]);
+
+    return null;
+}
